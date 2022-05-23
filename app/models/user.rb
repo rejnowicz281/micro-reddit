47514ed8a719ea76d4bcdff7c8f052a59fa668a4
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-05-23 21:02:03 +0200

    Associate User with Posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, length: { maximum: 20 }, uniqueness: true
     validates :password, presence: true
+
+    has_many :posts
 end
