62441f3c0dbbea26f2e99808052d5a5a2358a599
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-05-23 20:22:42 +0200

    Generate Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_174307) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_181926) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
