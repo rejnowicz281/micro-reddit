a140b8e83ea473b53acd789154a691b650788188
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-05-23 21:56:45 +0200

    Generate Comment model, associate it with Posts and Users

---
+++ db/migrate/20220523194002_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :content
+
+      t.timestamps
+    end
+    
+    add_reference :comments, :user, null: false, foreign_key: true
+    add_reference :comments, :post, null: false, foreign_key: true
+  end
+end
